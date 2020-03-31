1dfc0d7c082eee763ad11f695bb94668ef455066
Harshit <harshitkawatra08@gmail.com>
2020-03-31 10:03:50 +0530

    added more functions

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+import math
 def add(num1,num2):
     return num1+num2
 def sub(num1,num2):
@@ -6,4 +7,18 @@ def mul(num1,num2):
     return num1 * num2
 def div(num1,num2):
     return num1/num2
+def pwr(num1,num2):
+    return num1**num2
+def modulus(num1,num2):
+    return num1%num2
+def root(num1,num2):
+    return num2**(1/num1)
+def ln(num1):
+    return math.log(num1)
+def sin(num1):
+    return math.sin(num1)
+def cos(num1):
+    return math.cos(num1)
+def tan(num1):
+    return math.tan(num1)
 
